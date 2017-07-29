cb0df661e3df248ca8fdddf9e04a5d06f9229845
Sean McPherson <seanmcp25@gmail.com>
2017-07-29 06:43:39 -0400

    Removed additional files. Organized images into separate folder.

---
+++ periodicTable.js
@@ -0,0 +1,182 @@
+const periodicTable = [
+  {
+    name: "Hydrogen",
+    symbol: "H",
+    atomicNumber: 1,
+    atomicMass: 1.008,
+    state: "gas",
+    famliy: "Nonmetal",
+    valenceE: 1,
+    valenceShell: "s"
+  },
+  {
+    name: "Helium",
+    symbol: "He",
+    atomicNumber: 2,
+    atomicMass: 4.003,
+    state: "gas",
+    famliy: "Noble Gas",
+    valenceE: 2,
+    valenceShell: "s"
+  },
+  {
+    name: "Lithium",
+    symbol: "Li",
+    atomicNumber: 3,
+    atomicMass: 6.941,
+    state: "solid",
+    famliy: "Alkali Metal",
+    valenceE: 1,
+    valenceShell: "s"
+  },
+  {
+    name: "Berylium",
+    symbol: "Be",
+    atomicNumber: 4,
+    atomicMass: 9.012,
+    state: "solid",
+    famliy: "Alkaline Earth Metal",
+    valenceE: 2,
+    valenceShell: "s"
+  },
+  {
+    name: "Boron",
+    symbol: "B",
+    atomicNumber: 5,
+    atomicMass: 10.811,
+    state: "solid",
+    famliy: "Metalloid",
+    valenceE: 1,
+    valenceShell: "p"
+  },
+  {
+    name: "Carbon",
+    symbol: "C",
+    atomicNumber: 6,
+    atomicMass: 12.011,
+    state: "solid",
+    famliy: "Nonmetal",
+    valenceE: 2,
+    valenceShell: "p"
+  },
+  {
+    name: "Nitrogen",
+    symbol: "N",
+    atomicNumber: 7,
+    atomicMass: 14.007,
+    state: "gas",
+    famliy: "Nonmetal",
+    valenceE: 3,
+    valenceShell: "p"
+  },
+  {
+    name: "Oxygen",
+    symbol: "O",
+    atomicNumber: 8,
+    atomicMass: 15.999,
+    state: "gas",
+    famliy: "Metalloid",
+    valenceE: 4,
+    valenceShell: "p"
+  },
+  {
+    name: "Fluorine",
+    symbol: "F",
+    atomicNumber: 9,
+    atomicMass: 18.998,
+    state: "gas",
+    famliy: "Halogen",
+    valenceE: 5,
+    valenceShell: "p"
+  },
+  {
+    name: "Neon",
+    symbol: "Ne",
+    atomicNumber: 10,
+    atomicMass: 20.180,
+    state: "gas",
+    famliy: "Noble Gas",
+    valenceE: 6,
+    valenceShell: "p"
+  },
+  {
+    name: "Sodium",
+    symbol: "Na",
+    atomicNumber: 11,
+    atomicMass: 22.990,
+    state: "solid",
+    famliy: "Alkali Metals",
+    valenceE: 1,
+    valenceShell: "s"
+  },
+  {
+    name: "Magnesium",
+    symbol: "Mg",
+    atomicNumber: 12,
+    atomicMass: 24.305,
+    state: "solid",
+    famliy: "Alkaline Earth Metals",
+    valenceE: 2,
+    valenceShell: "s"
+  },
+  {
+    name: "Aluminum",
+    symbol: "Al",
+    atomicNumber: 13,
+    atomicMass: 26.982,
+    state: "solid",
+    famliy: "Basic Metal",
+    valenceE: 1,
+    valenceShell: "p"
+  },
+  {
+    name: "Silicon",
+    symbol: "Si",
+    atomicNumber: 14,
+    atomicMass: 28.086,
+    state: "solid",
+    famliy: "Metalloid",
+    valenceE: 2,
+    valenceShell: "p"
+  },
+  {
+    name: "Phosphorus",
+    symbol: "P",
+    atomicNumber: 15,
+    atomicMass: 30.974,
+    state: "solid",
+    famliy: "Nonmetal",
+    valenceE: 3,
+    valenceShell: "p"
+  },
+  {
+    name: "Sulfur",
+    symbol: "S",
+    atomicNumber: 16,
+    atomicMass: 32.066,
+    state: "solid",
+    famliy: "Nonmetal",
+    valenceE: 4,
+    valenceShell: "p"
+  },
+  {
+    name: "Chlorine",
+    symbol: "Cl",
+    atomicNumber: 17,
+    atomicMass: 35.453,
+    state: "gas",
+    famliy: "Halogen",
+    valenceE: 5,
+    valenceShell: "p"
+  },
+  {
+    name: "Argon",
+    symbol: "Ar",
+    atomicNumber: 18,
+    atomicMass: 39.948,
+    state: "gas",
+    famliy: "Noble Gas",
+    valenceE: 6,
+    valenceShell: "p"
+  },
+];
